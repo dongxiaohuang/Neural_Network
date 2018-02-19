6c9907d9b139995280e848abc52abe489ac6b1e8
Zheng Xun Phang <zheng.x.phang17@imperial.ac.uk>
2018-02-19 22:52:38 +0000

    Update classifiers.py

--- src/classifiers.py
+++ src/classifiers.py
@@ -1,13 +1,12 @@
 import numpy as np
 
-
 def softmax(logits, y):
     """
     Computes the loss and gradient for softmax classification.
 
     Args:
-    - logits: A numpy array of shape (N, C)
-    - y: A numpy array of shape (N,). y represents the labels corresponding to
+    - logits: NumPy array of shape (N, C)
+    - y: NumPy array of shape (N, ). y represents the labels corresponding to
     logits, where y[i] is the label of logits[i], and the value of y have a 
     range of 0 <= y[i] < C
 
@@ -15,19 +14,20 @@ def softmax(logits, y):
     - loss: Loss scalar
     - dlogits: Loss gradient with respect to logits
     """
-    loss, dlogits = None, None
+
+    X -= X.max(axis = 1, keepdims = True)     # For numerical stability
+    prob = np.exp(X)                          # Unnormalized probabilities
+    prob /= prob.sum(axis = 1, keepdims = True)     # Rows sum to 1
+    loss = np.log(prob)
+    loss = loss[np.arange(len(loss)), y].sum()
+
+    dlogits = None
+
     """
     TODO: Compute the softmax loss and its gradient using no explicit loops
     Store the loss in loss and the gradient in dW. If you are not careful
     here, it is easy to run into numeric instability. Don't forget the
     regularization!
     """
-    ###########################################################################
-    #                           BEGIN OF YOUR CODE                            #
-    ###########################################################################
 
-
-    ###########################################################################
-    #                            END OF YOUR CODE                             #
-    ###########################################################################
     return loss, dlogits
