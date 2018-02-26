90855b83be6fae6e1bf42280e4d965ce47ac46e2
Dongxiao Huang <dh4317@sprite02.doc.ic.ac.uk>
2018-02-26 15:11:51 +0000

    add plot for train_fcnet.py

--- src/train_fcnet.py
+++ src/train_fcnet.py
@@ -1,28 +1,42 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
 from src.fcnet import FullyConnectedNet
 from src.utils.solver import Solver
 from src.utils.data_utils import get_CIFAR10_data
 
 """
-TODO: Use a Solver instance to train a TwoLayerNet that achieves at least 50% 
+TODO: Use a Solver instance to train a TwoLayerNet that achieves at least 50%
 accuracy on the validation set.
 """
 ###########################################################################
-#                           BEGIN OF YOUR CODE                            #
+#                        38   BEGIN OF YOUR CODE                            #
 ###########################################################################
-
-model = FullyConnectedNet([10,10], reg=10)
+# [20,8], reg = 0.5
+model = FullyConnectedNet([50,8], reg = 0.3)
 solver = Solver(model, get_CIFAR10_data(),
                 update_rule='sgd',
                 optim_config={
                   'learning_rate': 1e-3,
                 },
                 lr_decay=0.95,
-                num_epochs=10, batch_size=100,
+                num_epochs=20, batch_size=100,
                 print_every=100)
 solver.train()
 
+plt.subplot(2,1,1)
+plt.title("Training loss")
+plt.plot(solver.loss_history, "o")
+plt.xlabel("Iteratition")
+
+plt.subplot(2,1,2)
+plt.title('Accuracy')
+plt.plot(solver.train_acc_history, "-o", label = 'train')
+plt.plot(solver.val_acc_history, "-o", label = 'val')
+plt.plot([0.45]* len(solver.val_acc_history), 'k--')
+plt.xlabel('Epoch')
+plt.legend(loc='lower right')
+plt.gcf().set_size_inches(15, 12)
+plt.show()
 
 ##############################################################################
 #                             END OF YOUR CODE                               #
