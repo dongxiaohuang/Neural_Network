0aa089f9137691c50d2a38d8a6dc0d01df94f5d9
Dongxiao Huang <dh4317@line03.doc.ic.ac.uk>
2018-02-28 13:22:54 +0000

    add function load data

--- src/utils/data_utils.py
+++ src/utils/data_utils.py
@@ -87,11 +87,24 @@ def get_FER2013_data(num_training=5, num_validation=6, num_test=2,
     it for classifiers. These are the same steps as we used for the SVM, but
     condensed to a single function.
     """
+    data = get_FER2013_Train_Test_data()
+    X_train = data['X_train'][0:num_training]
+    y_train = data['y_train'][0:num_training]
+    X_val = data['X_train'][num_training+1:num_training + num_validation +1]
+    y_val = data['y_train'][num_training+1:num_training + num_validation +1]
+    X_test = data['X_test'][0:num_test]
+    y_test = data['y_test'][0:num_test]
+
+    return {
+            'X_train': X_train, 'y_train': y_train,
+            'X_val': X_val, 'y_val': y_val,
+            'X_test': X_test, 'y_test': y_test,
+    }
+def get_FER2013_Train_Test_data():
     # Load the raw CIFAR-10 data
     fer_dir = '/vol/bitbucket/395ML_NN_Data/datasets/FER2013'
     fer_train_dir = '/vol/bitbucket/395ML_NN_Data/datasets/FER2013/Train'
     fer_test_dir = '/vol/bitbucket/395ML_NN_Data/datasets/FER2013/Test'
-    is_first = True
 
     # read file as dictionary
     label_dic = {}
@@ -101,84 +114,28 @@ def get_FER2013_data(num_training=5, num_validation=6, num_test=2,
         for line in f:
             (tag, label) = line.split(',')
             label_dic[tag] = int(label[0])
+    X_train = []
     y_train = []
     y_test = []
-    y_val = []
-    count_train = 1
     for f in os.listdir(fer_train_dir):
         fig_dir = fer_train_dir +'/'+ f
         fig = imread(name = fig_dir, mode = 'L')
-        if(count_train <= num_training):
-            X_train = imread(name = fig_dir, mode = 'L').tolist()
-            X_train = [X_train, fig]
-            y_train.append(label_dic["Test/"+str(f)])
-            count_train += 1
-        if count_train > num_training:
-            X_val = imread(name = fig_dir, mode = 'L').tolist()
-            X_val = [X_val, fig]
-            y_val.append(label_dic["Test/"+str(f)])
-            count_train += 1
-            if(count_train == num_training + num_validation):
-                X_train = np.array(X_train)
-                X_val = np.array(X_val)
-                break
-    #
-    is_first = True;
+        X_train.append(fig)
+        y_train.append(label_dic["Train/"+str(f)])
+
     X_test = []
-    count_test = 1
     for f in os.listdir(fer_test_dir):
         fig_dir = fer_test_dir +'/'+ f
         fig = imread(name = fig_dir, mode = 'L')
-        fig = imread(name = fig_dir, mode = 'L').tolist()
-        print(count_test)
-        print("test" ,num_test)
         X_test.append(fig)
         y_test.append(label_dic["Test/"+str(f)])
-        count_test += 1
-        if count_test > num_test :
-            X_test = np.array(X_test)
-            break
 
-    print("training==================")
-    print(X_train.shape, y_train)
-    print("testing==================")
-    print(X_test.shape)
-    print(y_test)
-    print("val==================")
-    print(X_val.shape, y_val)
-    # print(data.ix[0:5,:].to_dict())
-
-        # X_train.append(Image.open(os.path.join(path,f))
-
-    # X_train, y_train, X_test, y_test = load_CIFAR10(cifar10_dir)
-    #
-    # # Subsample the data
-    # mask = list(range(num_training, num_training + num_validation))
-    # X_val = X_train[mask]
-    # y_val = y_train[mask]
-    # mask = list(range(num_training))
-    # X_train = X_train[mask]
-    # y_train = y_train[mask]
-    # mask = list(range(num_test))
-    # X_test = X_test[mask]
-    # y_test = y_test[mask]
-    #
-    # # Normalize the data: subtract the mean image
-    # if subtract_mean:
-    #     mean_image = np.mean(X_train, axis=0)
-    #     X_train -= mean_image
-    #     X_val -= mean_image
-    #     X_test -= mean_image
-    #
-    # # Transpose so that channels come first
-    # X_train = X_train.transpose(0, 3, 1, 2).copy()
-    # X_val = X_val.transpose(0, 3, 1, 2).copy()
-    # X_test = X_test.transpose(0, 3, 1, 2).copy()
-
-    # Package data into a dictionary
-    # return {
-    #   'X_train': X_train, 'y_train': y_train,
-    #   'X_val': X_val, 'y_val': y_val,
-    #   'X_test': X_test, 'y_test': y_test,
-    # }
-get_FER2013_data();
+    X_train = np.array(X_train)
+    X_test = np.array(X_test)
+    print(X_train, y_train)
+    print(X_test, y_test)
+    return {
+      'X_train': X_train, 'y_train': y_train,
+      'X_test': X_test, 'y_test': y_test,
+    }
+get_FER2013_Train_Test_data()
