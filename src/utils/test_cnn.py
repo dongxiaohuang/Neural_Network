a0f6af54d8f4544423e36f334383d64baca5f428
Dongxiao Huang <dh4317@sprite04.doc.ic.ac.uk>
2018-03-07 16:35:39 +0000

    add explainations for test_cnn.py

--- src/utils/test_cnn.py
+++ src/utils/test_cnn.py
@@ -1,19 +1,25 @@
-from keras.models import Model # basic class for specifying and training a neural network
-from keras.utils import np_utils # utilities for one-hot encoding of ground truth values
 import numpy as np
-import os
+from os import listdir
 from scipy.misc import imread
-from sklearn.metrics import confusion_matrix
 from keras.models import load_model
 
 def test_deep_fer_model(img_folder, model= './utils/bestmodels/weights.23-1.20.hdf5'):
 ###########################################load model#####################
+"""
+    Given a folder with images, load the images and your best model to predict
+    the facial expression of each image.
+    Args:
+    - img_folder: Path to the images to be tested
+    Returns:
+    - preds: A numpy vector of size N with N being the number of images in
+    img_folder.
+"""
     test_model = load_model(model)
     preds = None
     ### Start your code here
     X_test = []
     print("loading data...")
-    for f in os.listdir(img_folder):
+    for f in listdir(img_folder):
         fig_dir = img_folder +'/'+ f
         fig = imread(name = fig_dir, mode = 'L').reshape(48,48,1)
         X_test.append(fig)
@@ -26,21 +32,4 @@ def test_deep_fer_model(img_folder, model= './utils/bestmodels/weights.23-1.20.h
     ### End of code
     return preds
 
-
-# score = model_resume.evaluate(X_test, Y_test, verbose=1)  # Evaluate the trained model on the test set!
-# Y_predict = model_resume.predict(X_test, batch_size=None, verbose=1, steps=None)
-# y_predict = np.argmax(Y_predict, axis=1)
-#
-# cm = confusion_matrix(y_test,y_predict)
-# print(cm)
-#rec_pre = pf.recall_precision_rates(num_classes, cm)
-#f1 = pf.fa_measure(1, num_classes, rec_pre)
-#cr = pf.all_classfi_rate(cm)
-
-#print(cr)
-#print(f1)
-
-# print('loss : %.2f'%score[0])
-# print('acc : %.2f'%score[1]*100)
-# checkpoint
 print (test_deep_fer_model('/vol/bitbucket/395ML_NN_Data/datasets/FER2013/Test'))
